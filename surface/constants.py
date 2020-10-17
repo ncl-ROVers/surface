5e4e072c0318fcb80123ebdd5526d304840a891e
TheCodeSummoner <kacper.florianski@gmail.com>
2020-10-17 11:17:03 +0100

    Added basic path constants and the ones loaded from .env file

---
+++ surface/constants.py
@@ -0,0 +1,19 @@
+"""
+Constants and other static values.
+"""
+import os
+import dotenv
+
+# Declare paths to relevant folders - tests folder shouldn't be known here
+ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), ".."))
+SURFACE_DIR = os.path.join(ROOT_DIR, "surface")
+RES_DIR = os.path.join(SURFACE_DIR, "res")
+LOG_DIR = os.path.join(SURFACE_DIR, "log")
+
+# Load the environment variables from the root folder and/or the resources folder
+dotenv.load_dotenv(dotenv_path=os.path.join(ROOT_DIR, ".env"))
+dotenv.load_dotenv(dotenv_path=os.path.join(RES_DIR, ".env"))
+
+# Declare redis settings - use .env file to override the defaults
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
