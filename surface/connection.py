e73f66d648a77fe7a2b9876cff6d9293b6792265
TheCodeSummoner <kacper.florianski@gmail.com>
2020-12-19 17:05:15 +0000

    Added some docs, IDLE status for the TCP connection, and adjusted some logging

--- surface/connection.py
+++ surface/connection.py
@@ -1,5 +1,5 @@
 """
-TODO
+Connection module for exchanging data with the Raspberry Pi.
 """
 import msgpack
 from msgpack import PackException
@@ -16,12 +16,20 @@ from .exceptions import NetworkingException
 # noinspection PyMethodParameters
 class Connection:
     """
-    todo
+    Communicate with Raspberry Pi by establishing a 2-way data exchange via a TCP network.
+
+    A connection is a non-enforced singleton featuring the following functionalities:
+
+        - set and retrieve connection status
+        - network data using a socket
+        - connect to the server in a non-blocking manner
+        - communicate with the server in a separate process
+        - clean up resources
+
+    Upon the communication process ending, the `IDLE` connection status will be set. The calling code must then handle
+    this scenario, for example by `reconnect`-ing.
     """
     def __init__(self):
-        """
-        todo
-        """
         self._ip = CONNECTION_IP
         self._port = CONNECTION_PORT
         self._data_size = CONNECTION_DATA_SIZE
@@ -32,48 +40,53 @@ class Connection:
     @property
     def status(self) -> ConnectionStatus:
         """
-        todo
+        Retrieve current connection status (process-independent).
         """
         return ConnectionStatus(DataManager.connections[RK_CONNECTION_SURFACE_PI])
 
     @status.setter
     def status(self, value: ConnectionStatus):
         """
-        todo
+        Set the connection status (process-independent).
         """
         DataManager.connections[RK_CONNECTION_SURFACE_PI] = value
 
     @staticmethod
     def _new_socket() -> socket:
         """
-        todo
+        Build a new socket needed for networking purposes.
         """
         return socket()
 
     def _new_process(self) -> Process:
         """
-        todo
+        Create an new process needed for making communication a non-blocking, efficient operation.
         """
         return Process(target=self._communicate)
 
     def connect(self):
         """
-        todo
+        Connect to the server.
+
+        Runs all connection methods in a separate thread, to avoid hanging the main thread on the blocking operations.
+        """
+        Thread(target=self._connect_threaded).start()
+
+    def _connect_threaded(self):
+        """
+        Internal thread to connect to the server and start the communication process.
+
+        Connection may only happen if the client is currently disconnected.
+
+        Connection failures are silent and will not cause any global exceptions to be raised.
         """
         if self.status != ConnectionStatus.DISCONNECTED:
-            logger.error(f"Can't connect to {self._ip}:{self._port} - not disconnected (status is {self.status})")
+            logger.warning(f"Can't connect to {self._ip}:{self._port} - not disconnected (status is {self.status})")
             return
 
         logger.info(f"Connecting to {self._ip}:{self._port}")
         self.status = ConnectionStatus.CONNECTING
 
-        # Launch blocking operations in a separate thread
-        Thread(target=self._connect_threaded).start()
-
-    def _connect_threaded(self):
-        """
-        todo
-        """
         try:
             self._socket.connect(self._address)
             self._communication_process.start()
@@ -86,11 +99,16 @@ class Connection:
 
     def _communicate(self):
         """
-        todo
+        Exchange data with the server.
+
+        Within the loop, the client sends the data first, and then waits for a response. Once the loop is exited, the
+        `IDLE` status is set, and the calling code must detect and handle this on their own.
+
+        # TODO: Consider using a callback by passing a function to the connection class (and use it within this method)
         """
         while True:
             try:
-                # Send the data to the server and retrieve immediatelly after (2-way-communication)
+                # Send the data to the server and retrieve immediately after (2-way-communication)
                 self._socket.sendall(DataManager.transmission.all)
                 received_data = self._socket.recv(self._data_size)
 
@@ -114,12 +132,22 @@ class Connection:
                 logger.exception(f"An error occurred while communicating with the server")
                 break
 
+        # Once the communication has ended, the IDLE status will be set - should be detected and handled by the caller
+        self.status = ConnectionStatus.IDLE
+
     def disconnect(self):
         """
-        TODO
+        Disconnected from the server.
+
+        Disconnection may only happen if the client is connected.
+
+        Disconnection failures are silent and will not cause any global exceptions to be raised.
+
+        The `DISCONNECTED` status will always be set, regardless of whether the connection has been shut down properly
+        or not. This is to avoid the client being unable to connect again in case of non-handled issues.
         """
         if self.status != ConnectionStatus.CONNECTED:
-            logger.error(f"Can't disconnect from {self._ip}:{self._port} - not connected (status is {self.status})")
+            logger.warning(f"Can't disconnect from {self._ip}:{self._port} - not connected (status is {self.status})")
             return
 
         logger.info(f"Disconnecting from {self._ip}:{self._port}")
@@ -137,7 +165,7 @@ class Connection:
 
     def _cleanup(self, ignore_errors: bool = False):
         """
-        todo
+        Stop all components as well as recreate the socket and the communication process.
         """
         try:
             if self._communication_process.is_alive():
@@ -154,7 +182,7 @@ class Connection:
 
     def reconnect(self):
         """
-        todo
+        Disconnect and connect again.
         """
         logger.info(f"Reconnecting to {self._ip}:{self._port}")
         self.disconnect()
--- surface/enums.py
+++ surface/enums.py
@@ -1,14 +1,25 @@
 """
-todo
+Enumerations used in several places within the application.
 """
 from enum import Enum
 
 
 class ConnectionStatus(Enum):
     """
-    todo
+    Status of the data exchange process between some server and some client.
+
+    The general lifecycle of each connection is as follows:
+
+        - Disconnected
+        - Connecting
+        - Connected
+        - Idle (relevant to TCP communication - flow stopped but socket's still open)
+        - Disconnecting
+
+    The connection statuses will represent connections between all networking elements of the ROV.
     """
     CONNECTED = 0
     DISCONNECTED = 1
     CONNECTING = 2
     DISCONNECTING = 3
+    IDLE = 4
